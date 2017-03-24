57486b8477f4165fbe248fba41b2439f7491158f
Littielau <taqre@yandex.ru>
2017-03-24 22:03:24 +0300

    Create 2.c

---
+++ m1_2017/2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+int day=19;
+
+int month=3;
+
+printf("Data. Day: %i, month: %i\n", day, month);
+
+int c=day;
+
+int d=month;
+
+printf("New data. Day: %i, month: %i\n", day=d, month=c);
+
+
+return 0;
+
+}
