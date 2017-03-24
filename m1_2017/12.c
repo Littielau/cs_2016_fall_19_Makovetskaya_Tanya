6662e25f9e511dbb42833ecd3cac8cbe41e7cc74
Littielau <taqre@yandex.ru>
2017-03-24 22:26:15 +0300

    Create 12.c

---
+++ m1_2017/12.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+int max,d,z,i,x,b;
+
+
+char c[1000];
+
+gets(c);
+
+char a = c[1];
+
+b =(int)(a);
+
+max=b;
+
+for(i=1;c[i]!='\0';i++)
+
+{
+
+a = c[i];
+
+z=(int)(a);
+
+if (z>max)
+
+{
+
+max=z;
+
+
+}
+}
+
+printf("max= %i\n",max);
+
+return 0;
+
+}
