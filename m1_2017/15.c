6f0b68dbacfa60e78b0fba5a3cef86e45e75536f
Littielau <taqre@yandex.ru>
2017-03-25 16:12:43 +0300

    Create 15.c

---
+++ m1_2017/15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+int main()
+{
+    int i,j=0;
+    char a[1000], b[1000];
+    printf("Vvedite stroky\n");
+    gets(a);
+    b[j]=a[i];
+   for(i=0;a[i]!='\0';i++)
+ { if(a[i-1]!=a[i]){
+    b[j]=a[i];
+    printf("%c",b[j]);
+    j++;
+ }
+ } printf("\n");
+    return 0;
+}
