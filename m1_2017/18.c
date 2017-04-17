f1ecc8088f413d8f657a2984f5783cb970523e19
Littielau <taqre@yandex.ru>
2017-04-17 20:35:20 +0300

    Update 18.c

--- m1_2017/18.c
+++ m1_2017/18.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void copy(char *c,char *d)
-{ char str[100];
-FILE *C,*D;
-C=fopen(c,"r");
-D=fopen(d,"w");
-char i;
-while((i=fgetc(C))!=EOF)
-{
-fputc(i,D);
-}
+void copy(char *c,char *x)
+{   char str[100];
+    FILE *C,*X;
+    C=fopen(c,"r");
+    X=fopen(x,"w");
+    char i;
+    while((i=fgetc(C))!=EOF)
+    {
+        fputc(i,X);
+    }
 }
 int main()
 {
-char e[100],f[100];
-gets(e);
-gets(f);
-copy(e,f);
-return 0;
+    char a[100],b[100];
+    gets(a);
+    gets(b);
+    copy(a,b);
+    printf("Ok");
+    return 0;
 }
