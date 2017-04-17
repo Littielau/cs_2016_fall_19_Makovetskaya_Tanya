f2afa2420fba7f6ce9b7b13ce380d298d18a10dd
Littielau <taqre@yandex.ru>
2017-04-17 20:21:22 +0300

    Update 18.c

--- m1_2017/18.c
+++ m1_2017/18.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-void copy(char *c,char *x)
-{ char str[1000];
-FILE *C,*X;
 
+void copy(char *c,char *d)
+{ char str[100];
+FILE *C,*D;
 C=fopen(c,"r");
-X=fopen(x,"w");
+D=fopen(d,"w");
 char i;
 while((i=fgetc(C))!=EOF)
 {
-fputc(i,X);
+fputc(i,D);
 }
 }
-
 int main()
-{setlocale(LC_CTYPE, "Russian");
-char a[1000],b[1000];
-printf("Введите путь к расположению файла который копируют(C:\/Games\/filename1.txt)");
-gets(a);
-printf("Введите расположение файла куда копировать (C:\/Games\/filename2.txt)");
-gets(b);
-copy(a,b);
-printf("Сделано");
+{
+char e[100],f[100];
+gets(e);
+gets(f);
+copy(e,f);
 return 0;
 }
